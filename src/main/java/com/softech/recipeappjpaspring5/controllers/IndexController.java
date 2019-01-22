3cdb72d75a0ce7f66440bbe19af02e93f43028a6
Huynh <nguyenviethuynh1996.vih@gmail.com>
2019-01-22 16:05:26 +0700

    add notes recipe

---
+++ src/main/java/com/softech/recipeappjpaspring5/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.softech.recipeappjpaspring5.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+    private String index(){
+
+        return "index";
+    }
+
+}
